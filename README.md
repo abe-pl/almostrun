200382d051279734e4d32d0e5610f6a9da517891
Abe <abe@o2.pl>
2024-06-02 20:32:16 +0200

    Version 0.1
    Merged Almost Lost and Blood Run into a single map file
    Restored lights and models
    Added doorways
    Changed one Quad to Battle Suite
    Added support for almost all game modes (except Race)
    Rearranged weapons (depends on game mode)

---
+++ README.md
@@ -0,0 +1,28 @@
+# Almost Run
+
+This repository contains the QuakeLive map, which is a merge of Almost Lost and Blood Run.
+
+## Downloads
+
+The compiled version is available in the [Release section](https://github.com/abe-pl/almostrun/releases)
+
+## Screenshots
+
+![AlmostRun1](screenshots/shot0001.jpg)
+![AlmostRun2](screenshots/shot0002.jpg)
+![AlmostRun3](screenshots/shot0003.jpg)
+![AlmostRun4](screenshots/shot0004.jpg)
+![AlmostRun5](screenshots/shot0005.jpg)
+![AlmostRun6](screenshots/shot0006.jpg)
+![AlmostRun7](screenshots/shot0007.jpg)
+
+## Details
+
+Maps were imported from .bsp files and edited using [NetRadiant-custom](https://github.com/Garux/netradiant-custom)\
+All game modes are supported including CTF, One Flag, Harvester (except Race)\
+To build, check make.bat script
+
+## Credits
+
+* idSoftware for Almost Lost
+* Sten "ztn" Uusvali for Blood Run
---
+++ make.bat
@@ -0,0 +1,67 @@
+set RadiantPath="D:\Games\netradiant-custom-20240309"
+set EnginePath="D:\Games\wolfcamql12.6"
+set SteamCmdPath="D:\Games\steamcmd"
+
+@echo off
+if "%1" == "" goto usage
+if "%1" == "clean" goto clean
+if "%1" == "build" goto build
+if "%1" == "pack" goto pack
+if "%1" == "upload" goto upload
+goto end
+
+:usage
+echo requirements:
+echo - [https://github.com/Garux/netradiant-custom] 
+echo - [https://github.com/brugal/wolfcamql]
+echo - [pak00.pk3 from Quakelive\baseq3 to wolfcamql\baseq3]
+echo - [zz-q3netpack.pk3 from netradiant-custom\gamepacks\Q3.game to wolfcamql\baseq3]
+echo - [https://developer.valvesoftware.com/wiki/SteamCMD] only for upload to steam workshop
+echo edit make.bat
+echo set RadiantPath and EnginePath
+echo make.bat build
+echo make.bat pack
+echo "gl&hf"
+goto end
+
+:clean
+del src\maps\almostrun.bsp
+del src\maps\almostrun.srf
+del src\maps\almostrun.aas
+rd /s /q target
+del bspc.log
+goto end
+
+:build
+%RadiantPath%\q3map2.exe -game quakelive -fs_basepath %EnginePath% -fs_game baseq3 -meta src\maps\almostrun.map
+%RadiantPath%\q3map2.exe -game quakelive -fs_basepath %EnginePath% -fs_game baseq3 -vis src\maps\almostrun.map
+%RadiantPath%\q3map2.exe -game quakelive -fs_basepath %EnginePath% -fs_game baseq3 -light -bounce 8 -fastbounce -dark -dirtdepth 32 -dirtscale 3 -dirty -patchshadows -samples 3 -scale 1.2 -shade -cpma src\maps\almostrun.map
+%RadiantPath%\mbspc.exe -forcesidesvisible -optimize -bsp2aas src\maps\almostrun.bsp
+goto end
+
+:pack
+if not exist src\maps\almostrun.bsp (
+	echo Build map first: make.bat build
+	goto end
+)
+rd /s /q target
+rd /s /q content
+robocopy src target /e /xf *.map *.srf *.log
+powershell Compress-Archive -Force -Path target\* almostrun.zip
+mkdir content
+move /y almostrun.zip content\almostrun.pk3
+goto end
+
+:upload
+if "%SteamUser%"=="" (
+	echo set SteamUser=your_steam_login
+	goto end
+)
+if "%SteamPass%"=="" (
+	echo set SteamPass=your_steam_password
+	goto end
+)
+%SteamCmdPath%\steamcmd.exe +login %SteamUser% %SteamPass% +workshop_build_item almostrun.vdf +quit
+goto end
+
+:end
